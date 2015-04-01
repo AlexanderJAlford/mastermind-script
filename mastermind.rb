872e4926d964d6cb08da32544a1cd7bd75c0ac73
Alexander Alford <xander.alford@gmail.com>
2015-04-01 15:43:11 -0600

    add spec for inputting all peg colors

--- mastermind.rb
+++ mastermind.rb
@@ -1,9 +1,11 @@
 class Mastermind
   attr_reader :board
   attr_reader :turn
+  attr_reader :pegs
 
   def initialize
     @board = Array.new
+    @pegs = ['pink', 'purple', 'yellow', 'white', 'orange', 'green']
   end
 
   def input(piece1, piece2, piece3, piece4)
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../mastermind'
+require './mastermind'
 
 RSpec.describe Mastermind do
   context "while initializing"
@@ -6,10 +6,19 @@ RSpec.describe Mastermind do
       game = Mastermind.new
       expect(game.board).to be_empty
     end
-  context "after initializing"
-  it "accepts input of green pins" do
+  context "input"
+  it "accepts input of 4 green pegs" do
     game = Mastermind.new
     game.input('green', 'green', 'green', 'green')
     expect(game.board).to eq ['green', 'green', 'green', 'green']
+  end
+
+  it "accepts input of 4 pegs of all 6 colors" do
+    game = Mastermind.new
+    game.pegs.each do |color|
+      game.input(color, color, color, color)
+      expect(game.board).to eq [color, color, color, color]
+      game.board.clear
+    end
   end
 end
