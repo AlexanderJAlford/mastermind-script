afb94de77a64b63e19ee730d8c67224ce45dd499
Alexander Alford <xander.alford@gmail.com>
2015-04-14 10:30:03 -0600

    refactor tests more, add :clear option to .edit_board

--- mastermind.rb
+++ mastermind.rb
@@ -1,17 +1,17 @@
 class Mastermind
-  attr_reader(
-    :board,
-    :turncount,
-    :turn,
-    :pegs
-    )
-
   def initialize
     edit_board(:create)
     @turncount = 0
     @turn = [:move, :grade]
     @pegs = [:pink, :purple, :yellow, :white, :orange, :green]
   end
+
+  attr_reader(
+  :board,
+  :turncount,
+  :turn,
+  :pegs
+  )
 
   def edit_board(action, *pegs)
     case action
@@ -20,6 +20,9 @@ class Mastermind
     when :play
       @board[@turncount] = [pegs[0], pegs[1], pegs[2], pegs[3]]
       @turncount += 1
+    when :clear
+      self.edit_board(:create)
+      @turncount = 0
     end
   end
 
@@ -29,17 +32,5 @@ class Mastermind
         @pegs.push(peg) #inserts new peg at the end of @pegs
       end
     end
-  end
-
-  # def input(peg1, peg2, peg3, peg4)
-  #   @board[@turncount] = [peg1, peg2, peg3, peg4]
-  #   @turncount += 1
-  # end
-
-  def newgame
-    @board = Array.new(10, Array.new)
-    @turncount = 0
-    @turn = [:move, :grade]
-    @pegs = [:pink, :purple, :yellow, :white, :orange, :green]
   end
 end
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -1,42 +1,6 @@
 require './mastermind'
 
 describe Mastermind do
-  
-  # describe '.new' do
-  #   let(:board) {Mastermind.new.board_gen}
-  #   it 'creates an empty gameboard of ten arrays' do
-  #     expect(board).to eq [[], [], [], [], [], [], [], [], [], []]
-  #   end
-  # end
-  
-  # describe '.input' do
-
-  #   context 'when 4 green pegs are passed to .input' do
-
-  #     it 'accepts the input and updates one row of the board accordingly' do
-  #       game = Mastermind.new
-  #       game.input(:green, :green, :green, :green)
-  #       expect(game.board).to eq [[:green, :green, :green, :green], [], [], [], [], [], [], [], [], []]
-  #     end
-  #   end
-
-  #   it 'accepts input of 4 pegs of all 6 colors' do
-  #     game = Mastermind.new
-  #     game.pegs.each do |color|
-  #       10.times do |i|
-  #         game.input(color, color, color, color)
-  #         expect(game.board[i]).to eq [color, color, color, color]
-  #       end
-  #       game.newgame
-  #     end
-  #   end
-  # end
-
-  # describe '.board_gen' do
-  #   subject{ Mastermind.new.board_gen }
-  #   it { is_expected.to eq [[], [], [], [], [], [], [], [], [], []] }
-  # end
-
   describe '.edit_board' do
     let(:empty_board) {Array.new(10) {Array.new(4)}} #Ten arrays of four within an array
     let(:game) { Mastermind.new }
@@ -49,6 +13,18 @@ describe Mastermind do
       it 'plays args 2-5 as a guess' do
         game.edit_board(:play, :green, :purple, :orange, :yellow)
         expect(game.board[0]).to eq [:green, :purple, :orange, :yellow]
+      end
+      it 'ignores args 6 and above' do
+        game.edit_board(:play, :purple, :yellow, :pink, :white, :green, :purple, :green)
+        expect(game.board[0]).to eq [:purple, :yellow, :pink, :white]
+      end
+    end
+    context '(:clear)' do
+      it 'regenerates @board and resets @turncount' do
+        game.edit_board(:play, :green, :purple, :orange, :yellow)
+        game.edit_board(:clear)
+        expect(game.board).to eq empty_board
+        expect(game.turncount).to eq 0
       end
     end
   end
@@ -64,4 +40,7 @@ describe Mastermind do
       expect(game.pegs).not_to eq [:pink, :purple, :yellow, :white, :orange, :green, 'brown']
     end
   end
+  describe '.new_game' do
+    subject { Mastermind.new }
+
 end
